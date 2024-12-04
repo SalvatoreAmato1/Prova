e91f7f42dd0e567218ff34e053d5526482f28db9
claud <claud@172.19.240.85>
2024-12-04 14:06:14 +0100

    opfile1

--- src/main/java/Rubrica/OpFile.java
+++ src/main/java/Rubrica/OpFile.java
@@ -4,11 +4,24 @@
  * and open the template in the editor.
  */
 package Rubrica;
-
+import Rubrica.ElencoContatti;
 /**
  *
  * @author mario
  */
 public class OpFile {
+    private String filename ;
+
+    public OpFile(String filename) {
+        this.filename = filename;
+    }
+            
+    public void scrivi(ElencoContatti c, String filename){
+        
+    }
     
+    public ElencoContatti leggi(String filename){
+        ElencoContatti lista  = new ElencoContatti();
+        return lista;
+    }
 }
