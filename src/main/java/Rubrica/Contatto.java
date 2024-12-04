e2321deda67f449c8400b1e6693163c96e5e9868
super <super@172.19.133.183>
2024-12-04 10:05:07 +0100

    ciaoo

---
+++ src/main/java/Rubrica/Contatto.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rubrica;
+
+
+/**
+ *
+ * @author super
+ */
+public class Contatto {
+    private String nome;
+    private String cognome;
+    private String[] numeri;
+    private String[] mail;
+    private static final int MAX_NUMERI = 3;
+    private static final int MAX_EMAIL = 3;
+    
+    
+    public Contatto(String nome, String cognome) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.numeri = new String[MAX_NUMERI];
+        this.mail = new String[MAX_EMAIL];
+}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public String[] getNumeri() {
+        return numeri;
+    }
+
+    public String[] getMail() {
+        return mail;
+    }
+    
+}
